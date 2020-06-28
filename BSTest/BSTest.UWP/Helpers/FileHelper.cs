0496c570c8e48b8b40c3c29274926ef4cfcbe902
Henry Moreno <hlmoreno@gmail.com>
2020-06-28 13:52:14 -0500

    Ajustes proyecto UWP

---
+++ BSTest/BSTest.UWP/Helpers/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using BSTest.Droid.Helpers;
+using BSTest.Data;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace BSTest.Droid.Helpers
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string path = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            return Path.Combine(path, filename);
+        }
+    }
+}
